d61e0d8ff1ff4ccf1757931e547421bf61592e88
Emediong Bassey Edem <edememediong1@gmail.com>
2024-09-21 02:26:36 +0100

    working with the utils

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,12 +1,14 @@
-
+import {calculateInvestmentResults} from '../util/investment.js'
 
 function Results({input}) {
+  const resultsData = calculateInvestmentResults(input);
+
+  console.log(resultsData)
   return (
     <div>
       Results
       {console.log(input)}
     </div>
-    
   )
 }
 
